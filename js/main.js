31c5e18074c9f33e3a08062ba30c4f4d2acc9139
문채영 <s2123@e-mirim.hs.kr>
2022-08-19 09:52:57 +0900

    반응형 nav 완성

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
 const toggleBtn = document.querySelector('.navbar-toggle-btn');
-const menu = document.querySelector('navbar-menu');
+const menu = document.querySelector('.navbar-menu');
 
 toggleBtn.addEventListener('click', ()=> {
     menu.classList.toggle('active');   // 클릭 시 active 없으면 active 추가
 })
+
+function random(min, max) {
+    return parseFloat((Math.random * (max-min)+min).toFixed(2))
+}
+
+function floatingCircle(item, delayAfter, size) {
+    gsap.to(
+        item,
+        random(1.5, 2.5),
+        {
+            delay: random(0, delayAfter), //몇초 뒤에 애니메이션을 실행, 지연 시간 설정
+            y: size, //transform: translateY(수치)와 같음. 수직으로 움직이는 크기
+            repeat: -1, //몇 번 반복하는지를 설정, -1은 무한 반복.
+            yoyo: true, //한번 재생된 애니메이션을 다시 뒤로 재생-위아래 움직임
+            ease: Power1.easeInOut //easing 함수 적용
+        }
+    )
+}
+
+floatingCircle('.c', 1, 25)
 
